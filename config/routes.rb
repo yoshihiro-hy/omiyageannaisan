84331711d3cb7466ec2dc13edbb357b5072448df
yoshihiro-hy <hy07140714@gmail.com>
2022-11-12 11:20:55 +0900

    Add: usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root to: 'staticpages#top'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
+  resource :users, only: [:new, :create]
 end
